b57c8db8534828b9bcc7b90c7cfd562962acc21a
xipengyang <xipengyang@gmail.com>
2019-09-15 22:39:36 +1200

    with styled-component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
-import * as React from 'react';
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 3rem;
+`;
 
 const Index = () => (
   <div>
-    <p>Hello Next.js.</p>
+    <Title>Hello Next.js</Title>
   </div>
 );
 
