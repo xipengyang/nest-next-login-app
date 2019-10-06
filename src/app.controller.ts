6c79b95a5c664f8aa4034d450123bac842d8e277
xipengyang <xipengyang@gmail.com>
2019-10-06 23:34:39 +1300

    updated express version

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Render('index')
-  public index() {
+  public index(@Res() res: {render: any}) {
     return {};
   }
 }
